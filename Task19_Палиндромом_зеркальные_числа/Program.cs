86b52a2948988abcdd7c8821d6024b240e9069fc
Dmitry Buyanov <tanker_34-85@mail.ru>
2023-10-16 16:07:00 +0300

    Задачи 3я лекция

--- Task19_Палиндромом_зеркальные_числа/Program.cs
+++ Task19_Палиндромом_зеркальные_числа/Program.cs
@@ -10,12 +10,6 @@ using System.ComponentModel;
 System.Console.Write("Введи пятизначное число: ");
 int userNambr = Convert.ToInt32(Console.ReadLine());
 
-if (userNambr < 9999 || userNambr > 100000)
-{
-    System.Console.WriteLine("Читай условие!"); // При вводе неверных данный по мимо "Читай условие!" выводится еще и "False"
-    
-}
-
 int nambr1 = userNambr % 10;
 int nambr2 = userNambr / 10 % 10;
 int nambr3 = userNambr / 100 % 10;
@@ -24,7 +18,13 @@ int nambr5 = userNambr / 10000 % 10;
 
 int pcNambr = (nambr1 * 10000) + (nambr2 * 1000) + (nambr3 * 100) + (nambr4 * 10) +nambr5;
 
-if (userNambr == pcNambr)
+if (userNambr < 9999 || userNambr > 100000)
+{
+    System.Console.WriteLine("Читай условие!"); // При вводе неверных данный по мимо "Читай условие!" выводится еще и "False"
+    
+}
+
+else if (userNambr == pcNambr)
 {
     System.Console.WriteLine("True");
 }
