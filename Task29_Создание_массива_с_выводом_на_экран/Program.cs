3f04490855b97fbe2b8527b3e154d0b5946fc2a0
Буянов Дмитрий Юрьевич <Buyanov.D@panavto.ru>
2023-10-18 20:19:08 +0300

    Решено ДЗ к 4ому семинару 2.0

--- Task29_Создание_массива_с_выводом_на_экран/Program.cs
+++ Task29_Создание_массива_с_выводом_на_экран/Program.cs
@@ -3,7 +3,7 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
-int[] GetRnd(int size)
+int[] GetRnd()
 {
     int[] array = { 10, 80, 62, 48, 72, 54, 11, 31 };
     return array;
@@ -13,9 +13,9 @@ void PrintArray(int[] arr)
     Console.Write("[ ");
     for (int i = 0; i < arr.Length; i++)
     {
-        System.Console.Write(arr[i] + " ");
+        Console.Write(arr[i] + " ");
     }
     Console.Write("]");
 }
-int[] userArray = GetRnd(8);
+int[] userArray = GetRnd();
 PrintArray(userArray);
