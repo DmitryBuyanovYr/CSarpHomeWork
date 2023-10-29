5fb64599209becb4b4ed9133ac182620a1ff9f82
Буянов Дмитрий Юрьевич <Buyanov.D@panavto.ru>
2023-10-29 16:33:38 +0300

    задачи с 6ого семинара 2.0

---
+++ Task42_Десяточное_число_в_двоичное/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу которая преобразуеть 10тичное число в двоичное
+*/
+
+Console.Write("Введи число: ");
+double first = Convert.ToInt32(Console.ReadLine());
+
+int Binary(number)
+{
+    resalt = 0;
+    for (int i = number; i > 0; i /= 2)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            
+        }
+        resalt = resalt + number % i;
+    }
+}
+System.Console.WriteLine(int resalt = Binary(first));
