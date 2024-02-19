3e8e335a07aebd85408158302ba131bf378154eb
DmitryBuyanovYr <146630796+DmitryBuyanovYr@users.noreply.github.com>
2024-02-19 20:34:28 +0300

    Add files via upload

---
+++ 2Task01_Проверка на квадрат/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите програму, которая на вход 
+принимает 2 целых числа и проверяет являеться 
+ли первое число квадратом второго*/
+
+Console.Clear();
+System.Console.Write("Введи превое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введи второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Pow(secondNumber,2) == firstNumber)
+{
+    System.Console.WriteLine($"число {firstNumber} являеться квадратом числа {secondNumber}");
+}
+else if (Math.Pow(secondNumber,2) != firstNumber)
+{
+    System.Console.WriteLine($"число {firstNumber} не являеться квадратом числа {secondNumber}");
+}
+
+
+
+
+
+
+if (number1 == Math.Pow(number2,2))
+{
+Console.WriteLine("Квадрат числа {1} равен {0}", number1, number2); // новая фишка
+}
+else Console.WriteLine($"Квадрат числа {number2} НЕ равен {number1}");
+
+// Console.WriteLine("Квадрат числа " + number2 + " равен " + number1); // закоментить все ( Ctrl + /(У правого шифта))
+
+// Alt shift вниз дублировать 
---
+++ 2Task02 Числа от -N до N/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу которая принимает число N 
+// и выводит все целые числа от -N До N
+
+Console.Clear();
+
+System.Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -number; i < number + 1; i++)
+{
+    System.Console.Write(i);
+    if (i < number)
+    {
+        System.Console.Write(", ");
+    }
+}
